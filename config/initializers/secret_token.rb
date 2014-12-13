b3084f330448181c73c4d44d0dec05f17274b45c
Yoshiki <yoshiki.mc@gmail.com>
2014-12-12 17:21:18 -0800

    Initialize GiftFinder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftFinder::Application.config.secret_key_base = 'b8bf15158b04be0ab11912b352a9cdd21603b0eb034be6a2b1cbe174e4374deca4cbe0d65b353003d58fc327403eb00c551969b08d92089b39bdef07be380a4a'
